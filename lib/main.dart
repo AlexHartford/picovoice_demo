2a2575840857e897db79df523badd99698cac964
Alex Hartford <alex@oneblank.io>
2021-07-07 16:02:26 -0500

    Add fab, analysis options

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +10,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Picovoice Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        home: MyHomePage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
@@ -23,6 +29,33 @@ class MyHomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: MicFAB(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class MicFAB extends HookWidget {
+  const MicFAB({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isListening = useState(false);
+
+    return AvatarGlow(
+      animate: isListening.value,
+      glowColor: Theme.of(context).primaryColor,
+      endRadius: 60,
+      child: FloatingActionButton(
+        onPressed: () async {
+          isListening.value = !isListening.value;
+        },
+        child: Icon(
+          Icons.mic,
+          size: 32,
+        ),
+      ),
+    );
   }
 }
